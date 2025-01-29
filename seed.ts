ada0f0e27d1400a35f7d8e95bf37b70ca0d7429b
Lostovayne <moises.barrera.dev@gmail.com>
2025-01-29 19:54:48 -0300

    feat: add parseCsv fucntion

---
+++ seed.ts
@@ -0,0 +1,31 @@
+import csv from "csv-parser";
+import fs from "node:fs";
+
+interface Row {
+	text: string;
+}
+
+async function parseCsv(filePath: string): Promise<Row[]> {
+	return new Promise((resolve, reject) => {
+		const rows: Row[] = [];
+
+		fs.createReadStream(filePath)
+			.pipe(csv({ separator: "," }))
+			.on("data", (row: Row) => {
+				rows.push(row);
+			})
+			.on("error", (error: Error) => {
+				reject(new Error(`Error parsing CSV file: ${error.message}`));
+			})
+			.on("end", () => {
+				resolve(rows);
+			});
+	});
+}
+
+const seed = async () => {
+	const data = await parseCsv("training_dataset.csv");
+	console.log(data);
+};
+
+seed();
